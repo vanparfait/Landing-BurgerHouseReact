7d26bb1dd8021a72145fdef97016e931ea63185a
vanparfait <nguemechiaparfait@gmail.com>
2024-07-21 08:13:01 +0100

    gestion de la marge du composant HeroTop

--- src/components/marketing/pageSections/HeroTop.jsx
+++ src/components/marketing/pageSections/HeroTop.jsx
@@ -6,9 +6,9 @@ import Header from "./Header";
 
 const HeroTop = () => {
   return (
-    <div className="bg-primary w-full h-screen">
+    <div className="bg-primary w-full">
       <div
-        className="bg-repeat w-full h-full"
+        className="bg-repeat w-full h-full pb-40"
         style={{ backgroundImage: `url(${bgFood})` }}
       >
         <Container>
